29ef069e8a2193243fb641a326aa3a48e236d31f
pengmin3@163.com <pengmin3@163.com>
2018-01-31 11:52:45 +0800

    resultEnum

---
+++ wechat-meal/src/main/java/com/meal/common/enums/ProductStatusEnum.java
@@ -0,0 +1,25 @@
+package com.meal.common.enums;
+
+/**
+ * Created by pengm on 2018/1/29.
+ */
+public enum  ProductStatusEnum {
+    UP(1,"上架"),
+    DOWN(2,"下架");
+
+    private Integer code;
+    private String message;
+
+    ProductStatusEnum(Integer code,String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
