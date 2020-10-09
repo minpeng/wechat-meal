31f2c7cc8fef8413d4cf00039d7b9b74cdb29420
pengm <Minpeng@users.noreply.github.com>
2020-10-09 16:16:35 +0800

    Update MyException.java
    补充异常msg

--- wechat-meal/src/main/java/com/meal/exception/MyException.java
+++ wechat-meal/src/main/java/com/meal/exception/MyException.java
@@ -7,15 +7,17 @@ import com.meal.common.enums.ResultEnum;
  */
 public class MyException extends RuntimeException {
     private Integer code;
-
+    private String  msg;
     public MyException(ResultEnum resultEnum) {
         super(resultEnum.getMessage());
 
         this.code = resultEnum.getCode();
+        this.msg = resultEnum.getMessage();
     }
 
     public MyException(Integer code, String message) {
         super(message);
         this.code = code;
+        this.msg = messgae;
     }
 }
